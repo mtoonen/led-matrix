9241e1c91cabb517425a3fd0784395c8024087c3
Meine Toonen <toonen.meine@gmail.com>
2021-08-17 11:52:35 +0200

    messagetype as string. added checkunprocessed endpoint

--- java/backend/src/main/java/nl/meine/led/backend/controllers/MessageController.java
+++ java/backend/src/main/java/nl/meine/led/backend/controllers/MessageController.java
@@ -1,13 +1,10 @@
 package nl.meine.led.backend.controllers;
 
-import javassist.NotFoundException;
 import nl.meine.led.backend.repositories.MessageRepository;
 import nl.meine.led.backend.resources.Message;
-import nl.meine.led.backend.resources.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("messages")
@@ -25,6 +22,12 @@ public class MessageController {
     @GetMapping("/unprocessed")
     public Message getFirstUnprocessed(){
         return repo.firstUnprocessed();
+    }
+
+    @GetMapping("/checkunprocessed")
+    public int checkUnprocessed(){
+        Message m= repo.firstUnprocessed();
+        return m != null ? 1 : 0;
     }
 
     @PostMapping("/save")
--- java/backend/src/main/java/nl/meine/led/backend/resources/MessageType.java
+++ java/backend/src/main/java/nl/meine/led/backend/resources/MessageType.java
@@ -1,11 +1,14 @@
 package nl.meine.led.backend.resources;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum MessageType {
 
     TEXT ("text"),
     TEXT_SCROLLING ("text_scrolling"),
     SHAPE ("shape"),
     UNDEFINED ("undefined");
+    @JsonValue
     private String type;
     MessageType(String type){
         this.type = type;
